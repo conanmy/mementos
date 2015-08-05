b574e85e9ad238e026ec7293c05d3565b8b93ecf
FrankMa <semstar2010@gmail.com>
2015-08-05 23:13:44 +1200

    remove nav for login

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -11,6 +11,7 @@
                 <div class="navbar-header">
                 <a href="/about" class="navbar-brand">Mementos</a>
                 </div>
+                @if (Auth::check())
                 <nav id="bs-navbar" class="collapse navbar-collapse">
                     <ul class="nav navbar-nav">
                         <li>
@@ -24,6 +25,14 @@
                         <li><a href="auth/logout">Logout</a></li>
                     </ul>
                 </nav>
+                @else
+                <nav id="bs-navbar" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="/auth/login">Login</a></li>
+                        <li><a href="/auth/register">Register</a></li>
+                    </ul>
+                </nav>
+                @endif
             </div>
         </header>
         <div class="container">
